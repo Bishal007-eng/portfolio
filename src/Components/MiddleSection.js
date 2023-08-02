acd1cac95b540570f99f54574109684d795b0e6b
Bishal Thapa <bishal.thap3@gmail.com>
2023-08-02 13:53:35 +0545

    Updated with Social media Section

--- src/Components/MiddleSection.js
+++ src/Components/MiddleSection.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactTyped from 'react-typed';
+import {HiArrowNarrowRight} from 'react-icons/hi';
+import { MdCloudDownload } from 'react-icons/md'
+
 // import { Typed } from './Typed'
 
 
@@ -7,26 +10,40 @@ export default function MiddleSection() {
   // const text = Typed("React Development" , [200]);
   return (
     <div className='text-white'>
-        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-          <p className='text-[#91ffcb] text-3xl font-bold p-2'>Hi there!</p>
-          <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 '>I'm Bishal Thapa</h1>
-          
-          <div className='flex justify-center items-center mt-2'>
-            <p className=' md:text-4xl sm:text-3xl text-xl font-bold py-4'>My skills </p>
-            <ReactTyped 
-              className='md:text-4xl sm:text-3xl text-xl font-bold md:pl-4 pl-2 text-[#91ffcb]'
-              strings={["React Development", "PHP","JavaScript" , "MySQL"]}
-              typeSpeed={100}
-              backSpeed={30}
-              loop
-            />
-          </div>
-          <p 
-            className='md:text-xl text-xl font-bold text-gray-500 mt-3'>
-            Hire me for a professional & most responsive web development with up-to-date technologies like ReactJs!
-          </p>
-          <button className='bg-[#91ffcb] w-[200px] rounded-md font-bold my-6 mx-auto py-3 text-black'>Download CV</button>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto flex flex-col justify-center'>
+        <p className='text-[#91ffcb] text-3xl font-bold p-2'>Hi there!</p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-2'>I'm Bishal Thapa</h1>
+        
+        <div className='flex items-center mt-2'>
+          <p className=' md:text-4xl sm:text-3xl text-xl font-bold py-2'>My skills </p>
+          <ReactTyped 
+            className='md:text-4xl sm:text-3xl text-xl font-bold md:pl-4 pl-2 text-[#91ffcb]'
+            strings={["React Development", "PHP","JavaScript" , "MySQL"]}
+            typeSpeed={100}
+            backSpeed={30}
+            loop
+          />
         </div>
+        <p 
+          className='md:text-xl text-xl font-bold text-gray-500'>
+          Hire me for a professional & most responsive web development with up-to-date technologies like ReactJs!
+        </p>
+        <div className='flex'>
+          <button 
+            className='text-white hover:text-black hover:bg-[#91ffcb] border-2 flex items-center rounded-md font-bold px-6 py-2 my-6 mr-4'>
+              Download CV &nbsp;<MdCloudDownload size={30}
+            />
+          </button>
+
+          <button 
+            className='text-white border-2 px-6 py-2 my-6 flex items-center rounded-md hover:bg-[#91ffcb] hover:text-black hover:font-bold'>
+              View Work&nbsp;<HiArrowNarrowRight size={30}
+            />
+          </button>
+
+        </div>
+          
+      </div>
     </div>
   )
 }
