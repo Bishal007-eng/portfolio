e824053505a4c637644a19811d2f51043899d09c
Bishal Thapa <bishal.thap3@gmail.com>
2023-08-02 17:16:22 +0545

    Dependencies updated

---
+++ src/Components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div className='lg:mt-[-120px] w-full h-screen text-white flex justify-center items-center p-4'>
+       <form action="" className='flex flex-col max-w-[600px] w-full'>
+
+          <div className='pb-8 text-center'>
+            <p className='text-[#91ffcb] text-4xl inline  border-b-4 border-[#91ffcb] font-bold sm:mx-[10px] lg:mx-auto mb-3'>Contact</p>
+            <p className='mt-2 font-bold text-xl py-4 text-gray-400'> Send me an Email for any Enquiry - bishal.thap3@gmail.com</p>
+          </div>
+          
+       </form>
+    </div>
+  )
+}
